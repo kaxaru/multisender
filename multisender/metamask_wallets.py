df7a3a17b595e2861ed4c7aa9027252dbef4b503
kaxaru <khidechi@gmail.com>
2022-02-19 16:56:44 +0300

    fix source

--- multisender/metamask_wallets.py
+++ multisender/metamask_wallets.py
@@ -2,12 +2,12 @@ from sys import stderr
 from os import system
 
 def get_main_wallet():
-    with open('./seed/main_wallet.txt', 'r') as file:
+    with open('../seed/main_wallet.txt', 'r') as file:
         _main_wallet = [row.strip() for row in file]
     return _main_wallet
 
 
 def get_wallets():
-    with open('./seed/wallets.txt', 'r') as file:
+    with open('../seed/wallets.txt', 'r') as file:
         _wallets = [row.strip() for row in file]
     return _wallets
